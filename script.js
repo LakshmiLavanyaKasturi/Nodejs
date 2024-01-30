f32d1eac9c53c352056d921bceb9bb11f38ec825
LakshmiLavanyaKasturi <lavanya.kasturi@repl.it>
2024-01-30 19:51:32 +0000

    Added script.js file for javascript configurations and updated index file to include headings

--- script.js
+++ script.js
@@ -12,13 +12,3 @@ class Person {
 
 const thatGuy = new Person("John Doe", "Software Engineer");
 thatGuy.print();
-function isPalindrome(num) {
-  const originalNum = num;
-  let reversedNum = 0;
-  while (num > 0) {
-    const digit = num % 10;
-    reversedNum = (reversedNum * 10) + digit;
-    num = Math.floor(num / 10);
-  }
-  return originalNum === reversedNum;
-}
